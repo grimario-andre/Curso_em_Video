697a364609eb98a90bcd7b4fc64c212c7bcedb67
grimario-andre <andre02souza@outlook.com.br>
2020-08-04 21:06:37 -0300

    class of  conditios.

---
+++ javascript/execicios.js
@@ -0,0 +1,23 @@
+//console.log("O console funcinou corretamente ");
+/*
+var vel = 78.2;
+
+console.log(`A velocidade do seu é: ${vel}km/h`);
+
+//condição simples.
+if (vel > 60) {
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando o cinto de segurança.`);
+
+    */
+
+//verificar vive ou não no Brasil.
+var pais = 'EUA';
+
+if (pais != 'EUA') {
+    console.log(`Você é estrangeiro`);
+} else {
+    console.log(`você é brasileiro`);
+}
