1480e914b03fb84bf01104d2197abdd77210737c
grimario-andre <andre02souza@outlook.com.br>
2020-08-17 20:33:46 -0300

    exercise tabuada

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,24 @@
 
-function contar(params) {
-    var inicio = window.document.getElementById('txti').value;
-    var fim = window.document.querySelector('input#txtf').value;
-    var passo = window.document.querySelector('input#txtp').value;
-    var res = window.document.getElementById('res');
+function tabuada(params) {
+    var num = window.document.getElementById('txtn').value;
+    var tab = window.document.querySelector('select#seltab');
 
-    //impedir de zerar a contagem.
-    if ((inicio.length == 0) || (fim.length == 0) || (passo.length == 0)) {
-        //window.alert('Há campo(s) vazio(s), preencha para continuar! ');
-        res.innerHTML = `Impossível contar`;
+    window.console.log('Verificar se o campo numero esta zerado');
+    if (parseInt(num.length) == 0) {
+        window.alert('Por favor digite um número. ');
     } else {
-        res.innerHTML = 'Contando ... <br>';
-        var i = parseInt(inicio);
-        var f = Number(fim);
-        var p = parseInt(passo);
+        var n = Number(num);
 
-        if (p <= 0) {
-            window.alert('Passo invalído! Considerando passo 1');
-            
-            p = 1;
-        }
+        var c = 0;
 
-        if (i < f) {
-            for (c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1F449}`;
-            }
-            //res.innerHTML = `\u{1F3F4}`;
-        } else {
-            for (c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} \u{1F449}`;
-            }
+        tab.innerHTML = '';
+
+        while (c <= 10) {
+            var item = window.document.createElement('option');
+            item.text = `${n} x ${c} = ${num*c}`;
+            item.value = `tab${c}`;
+            tab.appendChild(item);
+            c ++
         }
     }
 
